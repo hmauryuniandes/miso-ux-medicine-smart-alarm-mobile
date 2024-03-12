4f36c8cbc13b7d832882d399966e7d0d2748c5c4
Humberto <h.maury@uniandes.edu.co>
2024-03-11 20:43:12 -0500

    primer commit android app

---
+++ app/src/main/java/com/example/medicinesmartalarm/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.medicinesmartalarm
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.materialToolbar)
+        val materialShapeDrawable = toolbar.background as MaterialShapeDrawable
+        materialShapeDrawable.shapeAppearanceModel = materialShapeDrawable.shapeAppearanceModel
+            .toBuilder()
+            .setBottomLeftCorner(CornerFamily.ROUNDED, 40F)
+            .setBottomRightCorner(CornerFamily.ROUNDED, 40F)
+            .build()
+
+    }
+
+}
